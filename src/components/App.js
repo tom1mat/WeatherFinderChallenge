5e4485a4af3a07cf424fae6bb823301b821b9892
tom1mat <tomas.mateo@mercadolibre.com>
2020-12-05 13:31:49 -0300

    -Removing bootstrap, adding own styles. -Adding sass support.

--- src/components/App.js
+++ src/components/App.js
@@ -10,14 +10,15 @@ import WeatherContext from 'WeatherContext';
 const App = () => (
   <ErrorBoundary>
     <WeatherContext>
-      <main>
-        <Container className="col-5 title-container">
+      <main className="main">
+        <Container className="title-container">
           <h1 className="title-container__title">Weather Finder</h1>
           <h2 className="title-container__subtitle">
             Find out temperature, conditions and more...
           </h2>
+          <img className="title-container__image" src="bg.jpg" alt="Cover" />
         </Container>
-        <Container className="col-7 form-container">
+        <Container className="form-container">
           <Form />
           <WeatherInfo />
         </Container>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import './App.scss';
 
 import App from 'components/App';
 
